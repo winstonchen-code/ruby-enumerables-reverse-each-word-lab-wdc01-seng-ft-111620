f3200bdcec1cbee46cf6001a1d78761e0350271d
Winston Chen <winstonchen.wm@gmail.com>
2020-11-04 23:55:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(String)
    array = String.split("")
-   array.collect do |word|
-      word.reverse!
+   array.collect do {|word| word.reverse!}   
    end
    array.join(" ")
 end
