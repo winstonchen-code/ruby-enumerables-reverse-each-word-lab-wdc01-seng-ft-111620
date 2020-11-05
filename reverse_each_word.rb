4ffe2daf573f0b9da5ddaaafe9735436599b2213
Winston Chen <winstonchen.wm@gmail.com>
2020-11-05 00:00:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,3 @@
 def reverse_each_word(String)   #split each word in the str to reverse each word
-   array = String.split(" ")
-   reversed_string = []
-   array.collect do |word|
-      reversed_string << word.reverse
-   end
-   reversed_string.join(" ")
+   String
 end
